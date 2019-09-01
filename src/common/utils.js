7d9bfd56e3a13723c06c2f9d9556993170b4d6d2
thinking <1294873004@qq.com>
2019-09-01 17:22:50 +0800

    counting down

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,3 +2,7 @@ export function getImgUrl(url) {
   console.log('image url: ' , url)
   return url;
 }
+
+export function isFunction(functionToCheck) {
+  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+}
