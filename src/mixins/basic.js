26d97f748bc4a1dc5b03c73193e302d0764a4376
thinking <1294873004@qq.com>
2019-09-27 22:34:37 +0800

    member center

---
+++ src/mixins/basic.js
@@ -0,0 +1,24 @@
+export const basic = {
+  methods: {
+    // $emit(...args) {
+    //   this.triggerEvent(...args);
+    // },
+
+    getRect(selector, all) {
+      return new Promise(resolve => {
+        wx.createSelectorQuery()
+          .in(this)[all ? 'selectAll' : 'select'](selector)
+          .boundingClientRect(rect => {
+            if (all && Array.isArray(rect) && rect.length) {
+              resolve(rect);
+            }
+
+            if (!all && rect) {
+              resolve(rect);
+            }
+          })
+          .exec();
+      });
+    }
+  }
+};
