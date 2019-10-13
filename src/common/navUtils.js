c01e2630e3ca252c03608943ee527845873c8e9e
thinking <1294873004@qq.com>
2019-10-13 17:00:14 +0800

    page

--- src/common/navUtils.js
+++ src/common/navUtils.js
@@ -1,5 +1,10 @@
-export function navToPage(toUrl , query) {
-  console.log('nav to page : ' , toUrl)
+export function navToPage(toUrl , query = {}) {
+
+
+  const pages = getCurrentPages();
+  const curPage = pages[pages.length - 1];
+  const route = curPage.route;
+  query.referrer = route;
   if(query){
     toUrl = Object.keys(query).reduce((f , key , index)=>{
       return `${f}${index == 0 ? '' : '&'}${key}=${query[key]}`
@@ -8,6 +13,8 @@ export function navToPage(toUrl , query) {
   const nav = {
     url:toUrl
   }
+
+  console.log('nav to page : ' , nav)
   wx.navigateTo(nav)
 }
 export function redirectTo(toUrl) {
