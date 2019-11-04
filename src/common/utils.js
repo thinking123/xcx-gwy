89f06123d86a0140df5f370174e02fe99f1071ad
liangqing <liangq@weipaitang.com>
2019-11-04 21:04:02 +0800

    page

--- src/common/utils.js
+++ src/common/utils.js
@@ -108,6 +108,18 @@ export function isNumber(value) {
   return /^\d+$/.test(value);
 }
 
+export function formatTime(secs) {
+
+  var sec_num = parseInt(secs, 10)
+  var hours   = Math.floor(sec_num / 3600)
+  var minutes = Math.floor(sec_num / 60) % 60
+  var seconds = sec_num % 60
+
+  return [hours,minutes,seconds]
+    .map(v => v < 10 ? "0" + v : v)
+    // .filter((v,i) => v !== "00" || i > 0)
+    .join(":")
+}
 // export function range(num: number, min: number, max: number) {
 //   return Math.min(Math.max(num, min), max);
 // }
