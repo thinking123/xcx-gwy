8d8a0c67fe8b4ed6b1023a5fc9bd7b86954d669f
thinking <1294873004@qq.com>
2020-02-25 14:42:05 +0800

    vscode config

---
+++ src/common/console.js
@@ -0,0 +1,36 @@
+// export default class DebugConsole {
+//   open = false;
+//   oldConsole = null;
+//   constructor(open) {
+//     this.open = open;
+//     if (this.open) {
+//       this.oldConsole = this.console;
+//       this.console = {
+//         log: function() {},
+//         error: function() {}
+//       };
+//     }
+//   }
+// }
+
+let oldConsole = null;
+let newConsole = {};
+// const open = DEV;
+function noop() {}
+function debugConsole(open) {
+  if (open) {
+    oldConsole = console;
+    Object.getOwnPropertyNames(console).forEach(k => {
+      if (typeof console[k] === 'function') {
+        newConsole[k] = noop;
+      }
+    });
+    console = newConsole;
+
+    oldConsole.log(`debug : ${open ? 'closed' : 'opened'}`);
+  } else {
+    console.log(`debug : ${open ? 'closed' : 'opened'}`);
+  }
+}
+
+debugConsole(true);
--- wepy.config.js
+++ wepy.config.js
@@ -1,27 +1,28 @@
 const path = require('path');
+const DefinePlugin = require('@wepy/plugin-define');
+
 var prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   wpyExt: '.wpy',
   cliLogs: !prod,
-  build: {
-  },
+  build: {},
   resolve: {
     alias: {
       counter: path.join(__dirname, 'src/components/counter'),
       '@': path.join(__dirname, 'src'),
-      'components': path.join(__dirname , 'src/components'),
-      'store': path.join(__dirname , 'src/store/index.js'),
-      'UI': path.join(__dirname , 'src/components/UI'),
-      'Custom': path.join(__dirname , 'src/components/Custom'),
-      'icon': path.join(__dirname , 'static/icon'),
-      'image': path.join(__dirname , 'static/image'),
-      'utils': path.join(__dirname , 'src/common/utils.js'),
-      'baseUI': path.join(__dirname , 'src/css/baseUI.scss'),
-      'lessUI': path.join(__dirname , 'src/css/var.less'),
-      'basePage': path.join(__dirname , 'src/css/basePage.scss'),
-      'baseCss': path.join(__dirname , 'src/css/base.scss'),
-      'newBase': path.join(__dirname , 'src/css/newBase.scss'),
+      components: path.join(__dirname, 'src/components'),
+      store: path.join(__dirname, 'src/store/index.js'),
+      UI: path.join(__dirname, 'src/components/UI'),
+      Custom: path.join(__dirname, 'src/components/Custom'),
+      icon: path.join(__dirname, 'static/icon'),
+      image: path.join(__dirname, 'static/image'),
+      utils: path.join(__dirname, 'src/common/utils.js'),
+      baseUI: path.join(__dirname, 'src/css/baseUI.scss'),
+      lessUI: path.join(__dirname, 'src/css/var.less'),
+      basePage: path.join(__dirname, 'src/css/basePage.scss'),
+      baseCss: path.join(__dirname, 'src/css/base.scss'),
+      newBase: path.join(__dirname, 'src/css/newBase.scss')
     },
     aliasFields: ['wepy', 'weapp'],
     modules: ['node_modules'],
@@ -37,18 +38,22 @@ module.exports = {
     },
     babel: {
       sourceMap: true,
-      presets: [
-        '@babel/preset-env'
-      ],
+      presets: ['@babel/preset-env'],
       plugins: [
         '@wepy/babel-plugin-import-regenerator',
         '@babel/plugin-proposal-class-properties'
       ]
     }
   },
-  plugins: [],
+  plugins: [
+    DefinePlugin({
+      BASE_URL: JSON.stringify('http://foobar.com'),
+      // 'process.env.NODE_ENV': 'development',
+      // 'typeof window': JSON.stringify('undefined'),
+      DEV: true
+    })
+  ],
   appConfig: {
     noPromiseAPI: ['createSelectorQuery']
   }
-}
-
+};
